236e1b3f8ab9acf1c342549d04a80e1b2af4584b
Manish <md663744@gmail.com>
2022-03-04 08:24:56 +0530

    setting up the camera

--- src/App.js
+++ src/App.js
@@ -1,10 +1,56 @@
-import './App.css';
+//to import the required tensorflow,react-webcam,coco-ssd model,hooks dependencies
+import React, { useRef, useState, useEffect } from "react";
+import * as tf from "@tensorflow/tfjs";
+import * as cocossd from "@tensorflow-models/coco-ssd";
+import Webcam from "react-webcam";
+// import { drawRect } from "./utilities";
+import "./App.css";
+
+
+
 
 function App() {
+  const webcamRef = useRef(null);
+  const canvasRef = useRef(null);
+  
+  
+  
+  
+  
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Hello</h1> 
+        <h1>Hello</h1>
+        <Webcam
+          ref={webcamRef}
+          muted={true} 
+          style={{
+            position: "absolute",
+            marginLeft: "auto",
+            marginRight: "auto",
+            left: 0,
+            right: 0,
+            textAlign: "center",
+            zindex: 9,
+            width: 640,
+            height: 480,
+          }}
+        />
+
+        <canvas
+          ref={canvasRef}
+          style={{
+            position: "absolute",
+            marginLeft: "auto",
+            marginRight: "auto",
+            left: 0,
+            right: 0,
+            textAlign: "center",
+            zindex: 8,
+            width: 640,
+            height: 480,
+          }}
+        />
       </header>
     </div>
   );
